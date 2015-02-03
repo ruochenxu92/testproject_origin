c3a232aff325c60b4865064ea5db47a2ab4da495
xxu46 <xxu46@illinois.edu>
2015-02-02 18:28:27 -0600

    success

--- testproject/urls.py
+++ testproject/urls.py
@@ -6,6 +6,9 @@ from task import views
 urlpatterns = patterns('',
     #match list page with rule books, and will pass keyword args to views functions
     url(r'^tasks/$', views.ListTasksView.as_view()),
+    url(r'^$', views.ListTasksView.as_view()),
+
+
     url(r'^tasksUpdate/(?P<pk>[\w-]+)$', views.TaskUpdate.as_view()),
     url(r'^tasksDelete/(?P<pk>[\w-]+)$', views.TaskDelete.as_view()),
     url(r'^tasksCreate/$', views.TaskCreate.as_view()),
