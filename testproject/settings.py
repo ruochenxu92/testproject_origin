18d75b771b590a49e81686cdc4d7028a68fb7ec7
xxu46 <xxu46@illinois.edu>
2015-02-18 21:26:52 -0600

    All works now, please use your own email settings

--- testproject/settings.py
+++ testproject/settings.py
@@ -17,8 +17,8 @@ import os
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'x@gmail.com'
-EMAIL_HOST_PASSWORD = 'J4'
+EMAIL_HOST_USER = 'xx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xx'
 
 
 
