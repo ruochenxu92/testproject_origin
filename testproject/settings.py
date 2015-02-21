a7390ec4161466ae364284bbe22b60187661084c
xxu46 <xxu46@illinois.edu>
2015-02-21 11:17:55 -0600

    fail install solr because did not setup schema

--- testproject/settings.py
+++ testproject/settings.py
@@ -42,13 +42,17 @@ STATICFILES_FINDERS = (
 
 import os
 HAYSTACK_CONNECTIONS = {
+    # 'default': {
+    #     'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+    #     'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+    # },
+
     'default': {
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://127.0.0.1:8983/solr'
     },
 
 
-   
 }
 
 # Quick-start development settings - unsuitable for production
